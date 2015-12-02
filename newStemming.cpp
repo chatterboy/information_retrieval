ca33fa9430157ce64eae2a74561f422a651b597a
chatterboy <chatter0502@gmail.com>
2015-12-02 09:45:24 +0900

    ..

--- newStemming.cpp
+++ newStemming.cpp
@@ -1,144 +1,90 @@
 #include <cstdio>
 #include <cstring>
 
-// s[begin...end]
-struct Stemming {
+// ok
+// s => [0,k)
+// [0,j), [j,k)
+struct stemmer {
 	char * s;
-	int begin;
-	int end;
-	Stemming() {}
-	Stemming(char * s) : s(s) {
-		begin = 0;
-		end = strlen(s) - 1;
-	}
+	int j, k;
+	stemmer() {}
+	stemmer(char * s) : s(s) { k = strlen(s); }
 };
 
-// gets rid of plurals and -ed or -ing.
-// step1ab() pseudo code
-/*
-
-step1ab(string s)
-	if 
-
-*/
-
-
-int oldIsConsonant(char ch) {
-	switch (ch) {
-		case 'a': case 'e': case 'i': case 'o': case 'u':
-			return 0;
-		case 'y':
-			// need a process
-
-			break;
+int conso(stemmer * z, int i) {
+	switch (z->s[i]) {
+		case 'a': case 'e': case 'i': case 'o': case 'u': return 0;
+		case 'y' : return (i == 0) ? 1 : !conso(z, i - 1);
+		default: return 1;
 	}
+}
+
+int v(stemmer * z) {
+
+}
+
+int m(stemmer * z) {
+
+}
+
+// ok
+// string z->s ends with the string s.
+// ends(z, "ing")
+int ends(stemmer * z, char * s) {
+	int length = strlen(s);
+	if (length > z->k) return 0;
+	if (strcmp(z->s + z->k - length, s)) return 0;
+	z->j = z->k - length;
 	return 1;
 }
 
-int isConsonant(int i, Stemming & stm) {
-	switch (stm.s[i]) {
-		case 'a': case 'e': case 'i': case 'o': case 'u':
-			return 0;
-		case 'y':
-			// need a process
-			// 0이라고 해야하나 stm.begin 이라 해야하나
-			if (i-1 >= 0 && isConsonant(i-1, stm))
-				return 0;
+void step1ab(stemmer * z) {
+	if (z->s[z->k - 1] == 's') {
+		if (ends(z, "sses") || ends(z, "ies")) z->k -= 2;
+		else if (z->s[z->k - 2] != 's') z->k--;
+	}
+	if (ends(z, "eed")) {
+		if (m(z) > 0)
+			z->k--;
+	} else if (ends(z, "ed") || ends(z, "ing")) {
+		if (v(z)) {
+
+		}
+	}
+}
+
+void step2(stemmer * z) {
+	static const int n = 7;
+	static char * a[n] = {
+
+	};
+	static char * b[n] = {
+
+	};
+	int i;
+	for (i = 0; i < n; i++)
+		if (ends(z, a[i])) {
+			// 
 			break;
-	}
-	return 1;
+		}
 }
 
-int endWith(char * s, Stemming & stm) {
-	int n = strlen(s);
-	// this line needs some exceptions.
-	return strcmp(stm.s + stm.end - n + 1, s);
+void step3(stemmer * z) {
+	static const int n = 7;
+	static char * a[n] = {
+
+	};
+	static char * b[n] = {
+
+	};
+	int i;
+	for (i = 0; i < n; i++)
+		if (ends(z, a[i])) {
+			//
+			break;
+		}
 }
-
-/* step1a()
-
-sses -> ss
-ies -> i
-ss -> ss
-s ->
-
-*/
-// stm 좋은 이름 없을까
-void step1a(Stemming & stm) {
-	if (stm.s[stm.end] == 's') {
-		if (endWith("sses", stm) || endWith("ies", stm))
-			stm.end -= 2;
-		else if (endWith("s", stm))
-			stm.end--;
-	}
-}
-
-//*****************
-// -eed, -ed, -ing
-// step1b()
-//*****************
-void step1b(Stemming & stm) {
-	/* 줄이는 방법 없나 
-	if (endWith("eed", stm)) {
-		stm.end--;
-		if (!(m(stm) > 0))
-			stm.end++;
-	} else if (endWith("ed", stm) || endWith("ing", stm)) {
-		if (v(stm))
-	}
-	*/
-	if (endWith("eed", stm)) {
-		stm.end -= 3;
-		if (m(stm) > 0)
-			stm.end += 2;
-		else
-			stm.end += 3;
-	} else if (endWith("ed", stm)) {
-		stm.end -= 2;
-		if (!(v(stm)))
-			stm.end += 2;
-	} else if (endWith("ing", stm)) {
-		stm.end -= 3;
-		if (!v(stm)))
-			stm.end += 3;
-	}
-}
-
-//*****************
-// step 1c
-// 
-// (*v*) Y -> I
-//
-// e.g.
-//
-// happy -> happi
-// sky -> sky
-// 
-//*****************
-void step1c(Stemming & stm) {
-	if (endWith("y", stm)) {
-		stm.end--;
-		if (v(stm)) {
-			setToEnd("i", stm);
-		} else
-			stm.end++;
-	}
-}
-
-//*****************
-// step 1c
-// 
-// (*v*) Y -> I
-//
-// e.g.
-//
-// happy -> happi
-// sky -> sky
-// 
-//*****************
 
 int main() {
-	Stemming stm("processes");
-	printf("%d\n", endWith("sses", stm));
-	printf("%d\n", endWith("s", stm));
+	return 0;
 }
