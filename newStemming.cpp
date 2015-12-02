ebc1ea39e9bf6d09bce8d5ba0736189fcd214f50
chatterboy <chatter0502@gmail.com>
2015-12-02 11:46:10 +0900

    ..

--- newStemming.cpp
+++ newStemming.cpp
@@ -1,6 +1,9 @@
 #include <cstdio>
 #include <cstring>
 
+FILE * fin = fopen("input.txt", "r");
+FILE * fout = fopen("output.txt", "w");
+
 // ok
 // s => [0,k)
 // [0,j), [j,k)
@@ -8,23 +11,76 @@ struct stemmer {
 	char * s;
 	int j, k;
 	stemmer() {}
-	stemmer(char * s) : s(s) { k = strlen(s); }
+	stemmer(char * s) : s(s) {
+		k = strlen(s);
+		j = k;
+	}
 };
 
-int conso(stemmer * z, int i) {
+int cons(stemmer * z, int i) {
 	switch (z->s[i]) {
 		case 'a': case 'e': case 'i': case 'o': case 'u': return 0;
-		case 'y' : return (i == 0) ? 1 : !conso(z, i - 1);
+		case 'y' : return (i == 0) ? 1 : !cons(z, i - 1);
 		default: return 1;
 	}
 }
 
 int v(stemmer * z) {
-
+	int i;
+	int j = z->j;
+	for (i = 0; i < j; i++)
+		if (!cons(z, i))
+			return 1;
+	return 0;
 }
 
-int m(stemmer * z) {
+// cvc()
+// if i-2, i-1, i has the form consonant, vowel, consonant
+// this function returns true.
+int cvc(stemmer * z, int i) {
+	char ch;
+	if (i >= 2 && cons(z, i) && !cons(z, i-1) && cons(z, i-2)) {
+		ch = z->s[i];
+		if (ch != 'w' && ch != 'x' && ch != 'y')
+			return 1;
+	}
+	return 0;
+}
 
+// d() is 1 <=> i-1, i contains a double consonant.
+// d() is 0 <=> otherwise.
+int d(stemmer * z, int i) {
+	char * s = z->s;
+	if (i >= 1)
+		if (s[i-1] == s[i])
+			return 1;
+	return 0;
+}
+
+// we count the number of a consecutive consonants in string
+int m(stemmer * z) {
+	int n = 0;
+	int i = 0;
+	int j = z->j;
+	for (; i < j && cons(z, i); i++);
+	if (i >= j) return n;
+	for (;;) {
+		for (; i < j && !cons(z, i); i++);
+		if (i >= j) return n;
+		for (; i < j && cons(z, i); i++);
+		n++;
+		if (i >= j) return n;
+	}
+}
+
+// setto() 
+void setto(stemmer * z, char * s) {
+	int i;
+	int length = strlen(s);
+	int j = z->j;
+	for (i = 0; i < length; i++)
+		z->s[j + i] = s[i];
+	z->k = j + length;
 }
 
 // ok
@@ -32,13 +88,16 @@ int m(stemmer * z) {
 // ends(z, "ing")
 int ends(stemmer * z, char * s) {
 	int length = strlen(s);
-	if (length > z->k) return 0;
-	if (strcmp(z->s + z->k - length, s)) return 0;
-	z->j = z->k - length;
+	int k = z->k;
+	char * b = z->s;
+	if (length > k) return 0;
+	if (strcmp(b + k - length, s)) return 0;
+	z->j = k - length;
 	return 1;
 }
 
 void step1ab(stemmer * z) {
+	char ch;
 	if (z->s[z->k - 1] == 's') {
 		if (ends(z, "sses") || ends(z, "ies")) z->k -= 2;
 		else if (z->s[z->k - 2] != 's') z->k--;
@@ -46,11 +105,27 @@ void step1ab(stemmer * z) {
 	if (ends(z, "eed")) {
 		if (m(z) > 0)
 			z->k--;
-	} else if (ends(z, "ed") || ends(z, "ing")) {
-		if (v(z)) {
-
-		}
+	} else if ((ends(z, "ed") || ends(z, "ing")) && v(z)) {
+		z->k = z->j;
+		if (ends(z, "at"))
+			setto(z, "ate");
+		else if (ends(z, "bl"))
+			setto(z, "ble");
+		else if (ends(z, "iz"))
+			setto(z, "ize");
+		else if (d(z, z->k)) {
+			z->k--;
+			ch = z->s[z->k];
+			if (ch == 'l' || ch == 's' || ch == 'z')
+				z->k++;
+		} else if (m(z) == 1 && cvc(z, z->k))
+			setto(z, "e");
 	}
+}
+
+void step1c(stemmer * z) {
+	if (ends(z, "y") && v(z))
+		z->s[z->k] = 'i';
 }
 
 void step2(stemmer * z) {
@@ -85,6 +160,32 @@ void step3(stemmer * z) {
 		}
 }
 
+// this function tests the m() of a string is correct.
+// input the data from input.txt
+// output the data to output.txt
+void test() {
+	char s[101];
+	stemmer z;
+	for (;;) {
+		memset(s, 0, sizeof s);
+		if (fscanf(fin, "%s", s) == EOF) break;
+		z = stemmer(s);
+		fprintf(fout, "%-10s %2d\n", z.s, m(&z));
+	}
+	fclose(fin);
+}
+
+// this function tests the setto()
+void test2() {
+	char s[100] = "crack";
+	stemmer z(s);
+	printf("%s %d\n", z.s, z.k);
+	z.j--;
+	setto(&z, "df");
+	printf("%s %d\n", z.s, z.k);
+}
+
 int main() {
+	test2();
 	return 0;
 }
